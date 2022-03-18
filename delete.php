c43266086ccda5efe6ec8845fc333d3eeebadf45
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-03-18 18:25:05 +0200

    php aika valmis

--- delete.php
+++ delete.php
@@ -1,8 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Origin:*"  );
 header("Access-Control-Allow-Credentials:true");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type", "Access-Control-Allow-Header");
 header("Access-Control-Max-Age: 3600");
 header("content-type: application/json");
 
@@ -10,20 +10,25 @@ if ($_SERVER["REQUEST_METHOD"] === "OPTIONS"){
     return 0;
 }
 
+$input = json_decode(file_get_contents("php://input"));
+$id = filter_var($input->id,FILTER_SANITIZE_SPECIAL_CHARS);
 
 try {
 $db = new PDO("mysql:host=localhost;dbname=shoppinglist;charset=utf8","root","");
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-$sql = "Select * from item";
 
-$query = $db->query($sql);
+$query = $db->prepare("delete from item where id=(:id)");
+$query->bindValue(":id",$id,PDO::PARAM_STR);
+$query->execute();
 
 
 header("HTTP/1.1 200 OK");
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
-print json_encode($results);
+$data = array("id" => $id);
+print json_encode($data);
+
 } catch (PDOException $pdoex) {
 header("HTTP/1.1 500 Internal Server Error");
 $error = array("error" => $pdoex->getMessage());
 print json_encode($error);
 }
+?>
